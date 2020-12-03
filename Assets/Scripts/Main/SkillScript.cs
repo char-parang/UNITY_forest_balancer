125f33691c54b62f4dc9ebfc1bc655a25585971e
CharParang <char_blue@naver.com>
2020-12-03 13:58:23 +0900

    skill finish

--- Assets/Scripts/Main/SkillScript.cs
+++ Assets/Scripts/Main/SkillScript.cs
@@ -18,6 +18,7 @@ public class SkillScript : MonoBehaviour
         Debug.Log(data.getUserMoney());
         userMoney.text = data.getUserMoney().ToString();
         setSkillNames();
+        setSkillInfo();
         canBuy();
     }
 
@@ -30,6 +31,7 @@ public class SkillScript : MonoBehaviour
         skills[idx]++;
         data.setUserSkills(skills);
         setSkillNames();
+        setSkillInfo();
         canBuy();
     }
 
@@ -50,7 +52,13 @@ public class SkillScript : MonoBehaviour
 
     private void setSkillInfo()
     {
-
+        string[] col = { "Info" };
+        for(int i = 0; i < skills.Length; i++)
+        {
+            List<string> infos = data.selectData(col, "asset_Skill", "Num=" + i);
+            string[] splited = infos[0].Split('n');
+            skills[i].transform.Find("infoText").GetComponent<Text>().text = splited[0] + "<color=maroon> <i> <size=14pt>" + (skillLevels[i] * 3).ToString() + "%</size> </i> </color>" + splited[1];
+        }
     }
 
     private void canBuy()
