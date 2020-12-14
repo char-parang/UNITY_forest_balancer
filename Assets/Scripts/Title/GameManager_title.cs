a8cde52aa434b612c22b173b8786f42f5c417adb
CharParang <char_blue@naver.com>
2020-12-15 01:38:41 +0900

    bugfix

--- Assets/Scripts/Title/GameManager_title.cs
+++ Assets/Scripts/Title/GameManager_title.cs
@@ -2,47 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager_title : MonoBehaviour, ScrollControlable
+public class GameManager_title : MonoBehaviour
 {
     public GameObject[] popups;
     private GameObject[] clickables;
     private AudioSource bgm;
-    public Data data;
-    public GameObject village;
 
     // Start is called before the first frame update
     void Start()
     {
-        onMainScreenStart();
         bgm = gameObject.GetComponent<AudioSource>();
         bgm.volume = 0.5f;
-    }
-
-    private void onMainScreenStart()
-    {
-        for(int i =0;i < popups.Length; i++)
-        {
-            popups[i].SetActive(false);
-        }
-        clickables = GameObject.FindGameObjectsWithTag("Clickable");
-        setOtherClickable(true);
-    }
-
-    public void setOtherClickable(GameObject obj, bool status)
-    {
-        obj.GetComponent<Collider2D>().enabled = status;
-    }
-    public void setOtherClickable(bool status)
-    {
-        for (int i = 0; i < clickables.Length; i++)
-        {
-            if (clickables[i].GetComponent<Collider2D>() != null)
-                clickables[i].GetComponent<Collider2D>().enabled = status;
-        }
-    }
-
-    public void setAmount(float ratio, string code)
-    {
-        bgm.volume = ratio;
     }
 }
